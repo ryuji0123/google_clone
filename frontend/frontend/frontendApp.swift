3dc9e0104eac83e08f9ad94a417ac56b8960c798
ryuji0123 <ryo1011.javelin@gmail.com>
2022-07-05 23:03:29 +0900

    add xcode project for frontend

---
+++ frontend/frontend/frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  frontendApp.swift
+//  frontend
+//
+//  Created by ryo on 2022/07/05.
+//
+
+import SwiftUI
+
+@main
+struct frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
